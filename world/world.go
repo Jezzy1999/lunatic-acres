40570faca48a5e1c8a945ccce865d0552a6f87ec
crispg72 <chris.e.green@hotmail.com>
2021-10-12 19:45:15 +0100

    Init world and debug display map

--- world/world.go
+++ world/world.go
@@ -1,10 +1,9 @@
 package world
 
-
 func Initialise(width int, height int) [][]uint8 {
 	var worldMap = make([][]uint8, height)
 	for y := range worldMap {
-    	worldMap[y] = make([]uint8, width)
+		worldMap[y] = make([]uint8, width)
 	}
 	return worldMap
 }
