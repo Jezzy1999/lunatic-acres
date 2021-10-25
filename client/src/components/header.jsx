2f423b7d7c560b3d4e43ac323599bc1785813e9c
bg-my99 <bg.m99@outlook.com>
2021-10-25 14:08:57 +0100

    header components

---
+++ client/src/components/header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './header.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="header" >
+        Lunatic Acres
+      </div>
+    );
+  }
+}
+
+
+export default Header;
