d7e8155c8efa9eaea1093ec96f8d0de9d15e0c82
crispg72 <chris.e.green@hotmail.com>
2021-10-28 19:02:51 +0100

    Remove websocket, switch to functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,33 @@
-  import React, { Component } from 'react';
-  import { w3cwebsocket as W3CWebSocket } from "websocket";
+import React from 'react';
 import './App.css';
 
+import MessageRouterContext from './components/MessageRouter';
 import Header from './components/header';
 import WorldMap from './components/WorldMap';
 import PlayerInfo from './components/PlayerInfo';
 
-const client = new W3CWebSocket('ws://127.0.0.1:8080/ws');
+export const App = () => {
 
-class App extends Component {
-
-  state = {
-    height: 20,
-    width: 30,
-  };
-
-  componentDidMount() {
-    client.onopen = () => {
-      console.log('WebSocket Client Connected');
-    };
-    client.onmessage = (message) => {
-      console.log(message);
-    };
-  }
+  const height = 20
+  const width = 30
   
-  handleGameStart = () => {
-  }
-
-  render() {
-      const { height, width } = this.state;
-      return (
-          <div>
-            <Header />
-            <div className="game">
-                <div className="game-info">
-                    <div>
-                        <h4>Rules</h4>
-                        <p>How about some rules here?</p>
-                    </div>
-                </div>
-                <WorldMap height={height} width={width} socket={client}/>
-                <PlayerInfo socket={client}/>
-            </div>
+  return (
+      <MessageRouterContext>
+        <div>
+          <Header />
+          <div className="game">
+              <div className="game-info">
+                  <div>
+                      <h4>Rules</h4>
+                      <p>How about some rules here?</p>
+                  </div>
+              </div>
+              <WorldMap height={height} width={width}/>
+              <PlayerInfo/>
           </div>
-      );
-  }
+        </div>
+      </MessageRouterContext>
+  );
 }
 
 export default App;
