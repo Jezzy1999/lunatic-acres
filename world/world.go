ab02bdeb9d644ada7c5205c7a38b8f79d81baf54
crispg72 <chris.e.green@hotmail.com>
2021-10-30 17:42:01 +0100

    Load players in Initialise

--- world/world.go
+++ world/world.go
@@ -1,9 +1,39 @@
 package world
 
-func Initialise(width int, height int) [][]uint8 {
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+
+	"lunatic-acres/config"
+	"lunatic-acres/player"
+)
+
+var (
+	Players []player.Player
+)
+
+func Initialise(width int, height int, cfg config.Config) [][]uint8 {
 	var worldMap = make([][]uint8, height)
 	for y := range worldMap {
 		worldMap[y] = make([]uint8, width)
+	}
+
+	files, err := ioutil.ReadDir(cfg.Folders.Players)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, f := range files {
+		if !f.IsDir() {
+			newPlayer := player.Player{}
+			newPlayer.ReadFromFile(filepath.Join(cfg.Folders.Players, f.Name()))
+			Players = append(Players, newPlayer)
+		}
+	}
+
+	for _, p := range Players {
+		fmt.Printf("%v\n", p)
 	}
 	return worldMap
 }
