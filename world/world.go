449a6eb9976b046542749ba9dca293c437f07396
crispg72 <chris.e.green@hotmail.com>
2021-10-30 18:27:31 +0100

    Updated playerMessageListener for player login

--- world/world.go
+++ world/world.go
@@ -1,8 +1,10 @@
 package world
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+
 	"path/filepath"
 
 	"lunatic-acres/config"
@@ -42,7 +44,57 @@ func Initialise(cfg config.Config) [][]uint8 {
 	return worldMap
 }
 
+type MessageInfo struct {
+	MsgType string `json:"type"`
+	Payload string `json:"payload"`
+}
+
 // Receives messages of the type PLAYER_LOGIN
 func playerMessageListener(message string, replyChannel chan<- []byte) {
-	replyChannel <- []byte(message)
+
+	var msgInfo MessageInfo
+	if err := json.Unmarshal([]byte(message), &msgInfo); err != nil {
+		fmt.Printf("Couldnt parse json from %s", message)
+		return
+	}
+
+	switch msgInfo.MsgType {
+	case "PLAYER_LOGIN":
+		doPlayerLogin(msgInfo.Payload, replyChannel)
+	default:
+		doUnexpectedMsgType(msgInfo)
+	}
+}
+
+func doPlayerLogin(payload string, replyChannel chan<- []byte) {
+	type PlayerInfo struct {
+		Name string `json:"name"`
+	}
+	var playerInfo PlayerInfo
+	if err := json.Unmarshal([]byte(payload), &playerInfo); err != nil {
+		fmt.Printf("Couldnt parse json for player login from %s", payload)
+		return
+	}
+
+	for _, p := range Players {
+		if p.Name == playerInfo.Name {
+			type playerStats struct {
+				Money   int64 `json:"money"`
+				Seeds   int64 `json:"seeds"`
+				Produce int64 `json:"produce"`
+			}
+			statsToReturn := playerStats{}
+			str, err := json.Marshal(statsToReturn)
+
+			if err != nil {
+				fmt.Printf("Error converting player to json: %v", err)
+			} else {
+				replyChannel <- []byte(str)
+			}
+		}
+	}
+}
+
+func doUnexpectedMsgType(msgInfo MessageInfo) {
+	fmt.Printf("Unexpected message type: %s\n", msgInfo.MsgType)
 }
