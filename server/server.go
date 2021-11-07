791dfa76bb95be84320224d676ed3c6cd2419533
crispg72 <chris.e.green@hotmail.com>
2021-11-07 20:33:29 +0000

    We arent writing a chat app!

--- server/server.go
+++ server/server.go
@@ -90,13 +90,6 @@ func (c *Server) writePump() {
 			}
 			w.Write(message)
 
-			// Add queued chat messages to the current websocket message.
-			n := len(c.send)
-			for i := 0; i < n; i++ {
-				w.Write(newline)
-				w.Write(<-c.send)
-			}
-
 			if err := w.Close(); err != nil {
 				return
 			}
